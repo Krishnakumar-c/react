780c0495c1f674129c50f4b853828684e60dc4e1
Krishnakumar <krishna3917@gmail.com>
2018-02-09 11:36:56 +0530

    jsx

--- App.jsx
+++ App.jsx
@@ -3,7 +3,9 @@ class App extends React.Component {
     render(){
         return(
             <div>
-                Hello World!!!
+                <h1>Header</h1>
+                <h2>Content</h2>
+                <p>This is the content!!!</p>
             </div>
         )
     }
